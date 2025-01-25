478dc2ad1238604cdea24c99658c2dde7102c9a6
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2025-01-25 23:17:30 +0100

    add ContextMenuCommandBuilder as a valid type to `data` in Command interface

--- types/command.ts
+++ types/command.ts
@@ -1,11 +1,18 @@
-import type { ChatInputCommandInteraction, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder, SlashCommandSubcommandsOnlyBuilder } from "discord.js";
+import type {
+  ChatInputCommandInteraction,
+  ContextMenuCommandBuilder,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+} from "discord.js";
 import type { DiscordBot } from "../discord/client.ts";
 
 export interface Command {
   data:
     | SlashCommandBuilder
     | SlashCommandOptionsOnlyBuilder
-    | SlashCommandSubcommandsOnlyBuilder;
+    | SlashCommandSubcommandsOnlyBuilder
+    | ContextMenuCommandBuilder;
 
   run(
     interaction: ChatInputCommandInteraction,
