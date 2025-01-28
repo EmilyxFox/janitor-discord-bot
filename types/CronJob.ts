1d4975013c5fe53f2e080e42360b8d744b8f3529
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2025-01-28 20:42:54 +0100

    add jsdoc explanation to `runImmediately`

--- types/CronJob.ts
+++ types/CronJob.ts
@@ -1,6 +1,10 @@
 export interface CronJob {
   name: string;
   schedule: Deno.CronSchedule;
+  /**
+   * Whether to run the cron function immediately on startup.
+   * This might be useful for fetching data that needs to be available always and can't wait for the next time the cron runs.
+   */
   runImmediately: boolean;
   run(): void | Promise<void>;
 }
