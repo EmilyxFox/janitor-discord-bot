1b77edde5226ed129f084b104b64ab859c2a57fa
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2025-01-29 04:00:25 +0100

    add error handling to `respondToGoodBot`

--- discord/events/respondToGoodBot.ts
+++ discord/events/respondToGoodBot.ts
@@ -4,7 +4,9 @@ import { env } from "$utils/env.ts";
 export const respondToGoodBot = async (message: OmitPartialGroupDMChannel<Message<boolean>>) => {
   if (!message.reference?.messageId) return;
 
-  const repliedTo = await message.channel.messages.fetch(message.reference.messageId);
+  const repliedTo = await message.channel.messages.fetch(message.reference.messageId).catch((e) => console.log(e));
+  if (!repliedTo) return;
+
   if (repliedTo.author.id !== env.CLIENT_ID) return;
 
   if (message.content.toLowerCase() !== "good bot") return;
