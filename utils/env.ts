3cc6ee636c2921e39e33091152327408604ced68
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2025-01-30 06:53:51 +0100

    add LOGLEVEL to env.ts add `LOGLEVEL` to .env.example add ./logs/ to .gitignore

--- utils/env.ts
+++ utils/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   "DISCORD_TOKEN": z.string(),
   "CLIENT_ID": z.string(),
   "DEV": z.enum(["TRUE", "FALSE"]).optional().default("FALSE").transform((v) => v === "TRUE"),
+  "LOGLEVEL": z.enum(["ERROR", "WARN", "INFO", "HTTP", "VERBOSE", "DEBUG", "SILLY"]).optional(),
 });
 
 export const env = envSchema.parse(Deno.env.toObject());
