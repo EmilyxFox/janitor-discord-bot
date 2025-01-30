8fb1e07f0e9638493dcde598a40f1a60fa8114d9
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com.>
2025-01-30 02:24:32 +0100

    add DATABASE_URL env variable

--- utils/env.ts
+++ utils/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   "DISCORD_TOKEN": z.string(),
   "CLIENT_ID": z.string(),
   "DEV": z.enum(["TRUE", "FALSE"]).optional().default("FALSE").transform((v) => v === "TRUE"),
+  "DATABASE_URL": z.string().url(),
 });
 
 export const env = envSchema.parse(Deno.env.toObject());
