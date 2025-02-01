88632e55e563f80818a95f418ddf60aff539695a
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-02-01 06:17:37 +0100

    ✨Integrate logging functionality to replace console output on bot startup

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+import "./logging/logger.ts";
 import { DiscordBot } from "./discord/DiscordBot.ts";
 import { env } from "$utils/env.ts";
 import { attachGracefulShutdownListeners } from "$utils/gracefulShutdown.ts";
+import { getLogger } from "@logtape/logtape";
 
-console.log("Starting bot...");
+const log = getLogger(["system"]);
+
+log.info("Starting bot...");
 
 const bot = new DiscordBot({
   token: env.DISCORD_TOKEN,
