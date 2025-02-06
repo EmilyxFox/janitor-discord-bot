360180dafe1712ed125558f50bb2fdf18b33406d
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-02-06 22:42:38 +0100

    🔧 Refactor logging setup into a dedicated function for improved clarity and maintainability

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-import "./logging/logger.ts";
+import { setupLogging } from "./logging/logger.ts";
 import { DiscordBot } from "./discord/DiscordBot.ts";
 import { env } from "$utils/env.ts";
 import { attachGracefulShutdownListeners } from "$utils/gracefulShutdown.ts";
 import { getLogger } from "@logtape/logtape";
+await setupLogging();
 
 const log = getLogger(["system"]);
 
