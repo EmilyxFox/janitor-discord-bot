4e282f0af1f4a1e15ff6fc840a2bfcbd3ef80ad3
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-02-07 02:13:05 +0100

    🔧 Refactor EventHandler to support persistent handlers and improve event registration

--- discord/EventHandler.ts
+++ discord/EventHandler.ts
@@ -1,31 +1,53 @@
 import { Client, ClientEvents, Collection } from "discord.js";
 import { getLogger, withContext } from "@logtape/logtape";
 import { nanoid } from "nanoid";
+import { EventHandlerFunction } from "$types/EventHandler.ts";
+import { FindBlueskyHandles } from "$events/messageCreate/FindBlueskyHandles.ts";
+import { LogMessage } from "$events/messageCreate/LogMessage.ts";
+import { RespondToGoodBot } from "$events/messageCreate/RespondToGoodBot.ts";
+import { HandleNoGuilds } from "$events/ready/HandleNoGuilds.ts";
+import { HandleDisconnection } from "$events/shardDisconnect/HandleDisconnection.ts";
+import { BotLoggedInAndAvailble } from "$events/ready/BecomeAvailable.ts";
 
 const log = getLogger(["discord-bot", "event-handler"]);
 
-// Removing this any is too difficult for me :(
-// deno-lint-ignore no-explicit-any
-type EventHandlerMap<Event extends keyof ClientEvents> = Collection<Event, Array<(...args: any) => unknown>>;
+type EventHandlerMap<Event extends keyof ClientEvents> = Collection<Event, Array<EventHandlerFunction<Event>>>;
 
 export class EventHandler {
   private eventHandlers: EventHandlerMap<keyof ClientEvents>;
 
   constructor() {
     this.eventHandlers = new Collection();
+
+    this.addHandlers([
+      new FindBlueskyHandles(),
+      new LogMessage(),
+      new RespondToGoodBot(),
+      new BotLoggedInAndAvailble(),
+      new HandleNoGuilds(),
+      new HandleDisconnection(),
+    ]);
   }
 
   public handleEvent<Event extends keyof ClientEvents>(event: Event, ...args: ClientEvents[Event]) {
-    if (!this.eventHandlers.has(event)) return;
-
     const handlers = this.eventHandlers.get(event);
     if (!handlers) return;
 
+    const persistentHandlers: Array<EventHandlerFunction<keyof ClientEvents>> = [];
+
     for (const handler of handlers) {
-      // "Events" are very diverse so we invent our own context
       withContext({ id: nanoid() }, () => {
-        handler(...args);
+        handler.run(...args);
       });
+      if (!handler.runOnce) {
+        persistentHandlers.push(handler);
+      }
+    }
+
+    if (persistentHandlers.length > 0) {
+      this.eventHandlers.set(event, persistentHandlers);
+    } else {
+      this.eventHandlers.delete(event);
     }
   }
 
@@ -35,20 +57,21 @@ export class EventHandler {
 
     this.eventHandlers.each((handlers, event) => {
       amount += handlers.length;
-      handlersObject[event] = handlers.map((f) => f.name || "Anonymous function");
+      handlersObject[event] = handlers.map((f) => f.constructor.name);
 
-      // Register event listener on the Discord client
       discordClient.on(event, (...args) => this.handleEvent(event, ...args));
     });
 
     log.info("Registered {amount} event handlers", { handlers: handlersObject, amount });
   }
 
-  public registerEventHandler<Event extends keyof ClientEvents>(event: Event, listener: Array<(...args: ClientEvents[Event]) => unknown>) {
-    if (!this.eventHandlers.has(event)) {
-      this.eventHandlers.set(event, []);
-    }
+  public addHandlers(handlers: EventHandlerFunction<keyof ClientEvents>[]) {
+    for (const handler of handlers) {
+      if (!this.eventHandlers.has(handler.event)) {
+        this.eventHandlers.set(handler.event, []);
+      }
 
-    this.eventHandlers.get(event)?.push(...listener);
+      this.eventHandlers.get(handler.event)?.push(handler);
+    }
   }
 }
