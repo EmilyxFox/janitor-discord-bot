6a95ccba958d1429bb31d615071c1aff815bd0f2
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-02-11 07:55:09 +0100

    ✨ Add script to deregister global application commands

---
+++ exec/deregister-global-commands.ts
@@ -0,0 +1,36 @@
+import { REST, Routes } from "npm:discord.js";
+import { type RESTGetCurrentApplicationResult, RESTPutAPIApplicationCommandsResult } from "npm:discord-api-types/rest";
+
+const token = Deno.env.get("DISCORD_TOKEN");
+if (!token) {
+  console.error("No Discord token in env var.");
+  Deno.exit(1);
+}
+
+const discordREST = new REST().setToken(token);
+
+const me = await discordREST.get(Routes.currentApplication()) as RESTGetCurrentApplicationResult;
+
+if (!me.bot) {
+  console.error("No bot associated with application.");
+  Deno.exit(1);
+}
+
+const clientId = me.bot.id;
+if (!clientId) {
+  console.error("No client ID when registering commands");
+  Deno.exit(1);
+}
+discordREST
+  .put(Routes.applicationCommands(clientId), {
+    body: [],
+  })
+  .then((response: unknown) => {
+    const commands = response as RESTPutAPIApplicationCommandsResult;
+    if (commands.length <= 0) {
+      console.log("Successfully deregistered all global appliaction (/) commands");
+    }
+  })
+  .catch((err: unknown) => {
+    console.error("Error deregistering global application (/) commands", err);
+  });
