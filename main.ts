0bfd62660597f642c249919eed49a0bdd43175f6
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-02-11 03:37:33 +0100

    🗃️ Add migration for spoiler_enforced_channels table and update migration logic in main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,28 @@ import { setupLogging } from "./logging/logger.ts";
 import { DiscordBot } from "./discord/DiscordBot.ts";
 import { attachGracefulShutdownListeners } from "$utils/gracefulShutdown.ts";
 import { getLogger } from "@logtape/logtape";
+import { migrate } from "drizzle-orm/libsql/migrator";
+import { db } from "./database/database.ts";
 
 await setupLogging();
 
 const log = getLogger(["system"]);
+
+log.info("Migrating database...");
+
+try {
+  await migrate(db, {
+    migrationsFolder: "./database/drizzle",
+  });
+} catch (error) {
+  if (error instanceof Error) {
+    log.error(`Error migrating database: {errorMessage}`, {
+      errorMessage: `${error.name} ${error.message}`,
+      errorStack: error.stack,
+    });
+  }
+  Deno.exit(1);
+}
 
 log.info("Starting bot...");
 
