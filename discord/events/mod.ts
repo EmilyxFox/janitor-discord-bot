09a4d9aa532b6ff123e5d2860b79c2fe9d308350
EmilyxFox <48589793+EmilyxFox@users.noreply.github.com>
2025-02-28 21:08:20 +0100

    ♻️ (EventHandler): Refactor event handlers into a modular structure

---
+++ discord/events/mod.ts
@@ -0,0 +1,23 @@
+import { HandleButton } from "$discord/events/interactionCreate/HandleButton.ts";
+import { HandleCommand } from "$discord/events/interactionCreate/HandleCommand.ts";
+import { ConvertFToC } from "$discord/events/messageCreate/ConvertFToC.ts";
+import { EnforceSpoiler } from "$discord/events/messageCreate/EnforceSpoiler.ts";
+import { FindBlueskyHandles } from "$discord/events/messageCreate/FindBlueskyHandles.ts";
+import { RespondToGoodBot } from "$discord/events/messageCreate/RespondToGoodBot.ts";
+import { BotLoggedInAndAvailble } from "$discord/events/ready/BecomeAvailable.ts";
+import { HandleNoGuilds } from "$discord/events/ready/HandleNoGuilds.ts";
+import { StartCronJobs } from "$discord/events/ready/StartCronJobs.ts";
+import { HandleDisconnection } from "$discord/events/shardDisconnect/HandleDisconnection.ts";
+
+export const events = [
+  new HandleButton(),
+  new HandleCommand(),
+  new ConvertFToC(),
+  new EnforceSpoiler(),
+  new FindBlueskyHandles(),
+  new RespondToGoodBot(),
+  new BotLoggedInAndAvailble(),
+  new HandleNoGuilds(),
+  new StartCronJobs(),
+  new HandleDisconnection(),
+];
